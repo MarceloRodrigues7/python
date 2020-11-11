63a4ede66df53c82729d07e3fd3bf2935c366823
Marcelo Rodrigues <57776491+MarceloRodrigues7@users.noreply.github.com>
2020-11-10 21:21:24 -0300

    Add files via upload

---
+++ diceRandom.py
@@ -0,0 +1,23 @@
+import random
+
+listTrue = ["sim", "Sim", "SIM", "S", "s"]
+listFalse = ["nao", "não", "Nao", "Não", "NÃO", "NAO", "n", "N", "ñ", "Ñ"]
+
+def dice():
+    res = random.randrange(1, 7)
+    return res
+
+def ini():
+    start = input('Gostaria de jogar o dado? sim/nao')
+
+    while start in listTrue:
+        print('Resultado:', str(dice()))
+        start = input("Gostaria de jogar novamente? sim/nao")
+
+    if start in listFalse:
+        print('Ok, finalizando')
+
+    else:
+        print('Não entendi...')
+        ini()
+ini()
